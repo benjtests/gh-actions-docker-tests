51797a6901ea48750fc7fa54458e23b8960e52b1
BenjaminAtrium <blory@atrium-capital.com>
2025-09-16 08:36:55 +0200

    fixed typo

--- srv/app/src/Controller/HomeController.php
+++ srv/app/src/Controller/HomeController.php
@@ -11,6 +11,6 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return new Response('hello world!', 200);;
+        return new Response('hello world!', 200);
     }
 }
